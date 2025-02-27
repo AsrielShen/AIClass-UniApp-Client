805751fafc5301fbf109caf1a79f0a7f40c6403b
shen <1925326932@qq.com>
2025-02-27 16:27:09 +0800

    add Component block-bar

---
+++ components/block-bar/block-bar.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="block-bar">
+		{{ title }}
+	</view>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+  }
+})
+	
+</script>
+
+<style lang="less" scoped>
+.block-bar {
+	height: 75rpx;
+	line-height: 75rpx;
+	display: flex;
+	align-items: center;
+	border-bottom: 1rpx solid #cbcbcb;
+	&::before {
+		content: '';  /* 必须添加 content 属性才能显示伪元素 */
+		width: 10rpx;
+		height: 30rpx;
+		margin-right: 10rpx;
+		background-color: #39c5bb;
+	}
+}
+
+</style>
